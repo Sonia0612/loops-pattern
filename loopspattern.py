b1cae5b198dedf1232380dfdf4a6cc8c835ecba5
Sonia brahman <86123814+Sonia0612@users.noreply.github.com>
2021-09-12 19:32:20 +0530

    Add files via upload
    n=4
    1 2 3 4
    9 10 11 12
    13 14 15 16
    5 6 7 8
    
    if n=5(odd)
    1 2 3 4 5
    11 12 13 14 15
    21 22 23 24 25 
    16 17 18 19 20
    6 7 8 9 10

---
+++ loopspattern.py
@@ -0,0 +1,18 @@
+n=int(input())
+start=1
+
+for i in range(1,n+1):
+    for j in range(start,start+n):
+        print(j,end=" ")
+    print()
+    if i==(n+1)//2:
+        if n%2==0:
+            start=n*(n-1)+1
+        else:
+            start=n*(n-2)+1
+    elif i>(n+1)//2:
+        start=start-n*2
+    else:
+        start=start+n*2
+
+
